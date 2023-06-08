f3883d784644035e1d2899c3434b2339af734164
fredmbarros <fredmbarros@gmail.com>
2023-06-08 00:00:55 -0400

    VU meter panel done; still need to implement the needle

--- src/VuMeter.jsx
+++ src/VuMeter.jsx
@@ -1,5 +1,13 @@
+import VuMeterScale from "./resources/vu-meter.svg";
+
 const VuMeter = () => {
-  return <div className="vu-meter"></div>;
+  return (
+    <div className="vu-meter">
+      <img src={VuMeterScale} alt="VuMeter scale" className="vu-meter-scale" />
+      {/* <div className="vu-meter-scale"></div> */}
+      {/* <div className="vu-meter-scale-red"></div> */}
+    </div>
+  );
 };
 
 export default VuMeter;
